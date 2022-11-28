9e8527dc71f38d3d61f0bb63172c5437401897b2
dagurtomas <dagurtomas@gmail.com>
2022-11-28 15:46:55 +0000

    full proof of discreteness necessary condition modulo factorisation lemma

--- src/solid/colim_hom_injective.lean
+++ src/solid/colim_hom_injective.lean
@@ -8,31 +8,31 @@ universes u v
 noncomputable theory
 
 open category_theory
-open category_theory.limits 
+open category_theory.limits
 
-variables {C : Type u}  [category.{v} C] 
+variables {C : Type u}  [category.{v} C]
 variables {I : Type v} [small_category I] -- [is_cofiltered I]
 variables (X Y : C)
 variables (S : I ⥤ C) [has_limit S]
 
-def hom_as_cone_map : S.op ⋙ yoneda.obj Y ⋙ ulift_functor ⟶ 
-  (category_theory.functor.const Iᵒᵖ).obj 
+def hom_as_cone_map : S.op ⋙ yoneda.obj Y ⋙ ulift_functor ⟶
+  (category_theory.functor.const Iᵒᵖ).obj
   (ulift_functor.obj ((yoneda.obj Y).obj (opposite.op (limit S)))) :=
 { app := λ i, ulift_functor.map (as_hom (λ a, ((limit.cone S).π.app i.unop) ≫ a)),
   naturality' := begin
     intros i j ji,
     ext f,
     tidy,
-    have h₁ : as_hom (category_struct.comp (limit.π S j.unop)) (S.map ji.unop ≫ f) = 
+    have h₁ : as_hom (category_struct.comp (limit.π S j.unop)) (S.map ji.unop ≫ f) =
       (limit.π S j.unop) ≫ (S.map ji.unop ≫ f) := by refl,
-    have h₂ : as_hom (category_struct.comp (limit.π S j.unop ≫ S.map ji.unop)) f = 
+    have h₂ : as_hom (category_struct.comp (limit.π S j.unop ≫ S.map ji.unop)) f =
       (limit.π S j.unop ≫ S.map ji.unop) ≫ f := by refl,
     suffices h : (limit.π S j.unop) ≫ (S.map ji.unop) = (limit.π S i.unop),
     { rw ← h,
       rw h₁,
       rw h₂,
-      simp only [category_theory.limits.limit.w, 
-        category_theory.limits.limit.w_assoc, 
+      simp only [category_theory.limits.limit.w,
+        category_theory.limits.limit.w_assoc,
         eq_self_iff_true] },
     { simp only [category_theory.limits.limit.w, eq_self_iff_true] },
   end, }
@@ -41,12 +41,24 @@ def hom_as_cone : cocone (S.op ⋙ yoneda.obj Y ⋙ ulift_functor) :=
 { X := ulift_functor.obj ((yoneda.obj Y).obj (opposite.op (limit S))),
   ι := hom_as_cone_map Y S, }
 
-def can_map_from_colim_of_homs_to_hom_from_limit : 
-  colimit (S.op ⋙ yoneda.obj Y ⋙ ulift_functor) ⟶ ulift_functor.obj 
-  ((yoneda.obj Y).obj (opposite.op (limit S))) := 
+def can_map_from_colim_of_homs_to_hom_from_limit :
+  colimit (S.op ⋙ yoneda.obj Y ⋙ ulift_functor) ⟶ ulift_functor.obj
+  ((yoneda.obj Y).obj (opposite.op (limit S))) :=
   colimit.desc (S.op ⋙ yoneda.obj Y ⋙ ulift_functor) (hom_as_cone Y S)
 
-theorem can_is_injective (hI : ∀ i : I, epi ((limit.cone S).π.app i)) [is_cofiltered I] : 
+lemma can_eq_left_comp_with_proj {i : Iᵒᵖ}
+  (x : (S.op ⋙ yoneda.obj Y ⋙ ulift_functor).obj i) :
+  (limit.π S i.unop) ≫ x.down  =
+  (can_map_from_colim_of_homs_to_hom_from_limit Y S
+    (colimit.ι (S.op ⋙ yoneda.obj Y ⋙ ulift_functor) i x)).down :=
+begin
+  unfold can_map_from_colim_of_homs_to_hom_from_limit,
+  cases x,
+  simp only [category_theory.limits.types.colimit.ι_desc_apply],
+  refl,
+end
+
+theorem can_is_injective (hI : ∀ i : I, epi ((limit.cone S).π.app i)) [is_cofiltered I] :
   function.injective (can_map_from_colim_of_homs_to_hom_from_limit Y S) :=
 begin
   intros a b hab,
@@ -63,20 +75,16 @@ begin
   let pi := limit.π S i.unop,
   let pj := limit.π S j.unop,
   let pk := limit.π S k.unop,
-  -- have hi : epi pi := hI i.unop,
-  -- have hj : epi pj := hI j.unop,
   have hk : epi pk := hI k.unop,
-  have hcani : pi ≫ a'.down = (can_map_from_colim_of_homs_to_hom_from_limit Y S a).down,
-  { unfold can_map_from_colim_of_homs_to_hom_from_limit,
-    tidy },
-  have hcanj : pj ≫ b'.down = (can_map_from_colim_of_homs_to_hom_from_limit Y S b).down,
-  { unfold can_map_from_colim_of_homs_to_hom_from_limit,
-    tidy },
+  have hcani := can_eq_left_comp_with_proj _ _ a',
+  have hcanj := can_eq_left_comp_with_proj _ _ b',
   have hik : pi = pk ≫ (S.map ki.unop) := by tidy,
   have hjk : pj = pk ≫ (S.map kj.unop) := by tidy,
   have hpij : pi ≫ a'.down = pj ≫ b'.down,
   { rw hcani,
     rw hcanj,
+    rw ha,
+    rw hb,
     exact congr_arg ulift.down hab },
   rw hik at hpij,
   rw hjk at hpij,
@@ -90,3 +98,26 @@ begin
   exact hk.left_cancellation _ _ hpij,
 end
 
+instance is_iso_X_of_iso_cones {F : I ⥤ C} {c c' : cone F} (h : c ≅ c') : is_iso h.hom.hom :=
+begin
+  fconstructor,
+  use h.inv.hom,
+  split,
+  { have hom_comp : (h.hom.hom ≫ h.inv.hom) = (h.hom ≫ h.inv).hom := by refl,
+    have id_comp : 𝟙 c.X =
+      cone_morphism.hom (𝟙 c) := by refl,
+    rw hom_comp,
+    rw id_comp,
+    rw ← cone_morphism.ext_iff _ _,
+    exact h.hom_inv_id' },
+  { have hom_comp : (h.inv.hom ≫ h.hom.hom) = (h.inv ≫ h.hom).hom := by refl,
+    have id_comp : 𝟙 c'.X =
+      cone_morphism.hom (𝟙 c') := by refl,
+    rw hom_comp,
+    rw id_comp,
+    rw ← cone_morphism.ext_iff _ _,
+    exact h.inv_hom_id' },
+end
+
+def iso_X_of_iso_cones {F : I ⥤ C} {c c' : cone F} (f : c ≅ c') : c.X ≅ c'.X :=
+  as_iso f.hom.hom
--- src/solid/discrete.lean
+++ src/solid/discrete.lean
@@ -11,26 +11,33 @@ noncomputable theory
 
 universe u
 
-lemma to_Top_iso_limit (S : Profinite.{u}) : to_Top.obj S = limit (S.diagram ⋙ to_Top) :=
-begin
-  sorry,
-end
+lemma iso_limit₁ (S : Profinite.{u}) : (limit.cone S.diagram).X = limit S.diagram := by simp
 
-instance proj_epi {S : Profinite.{u}} (i : discrete_quotient S) :
-  epi ((limit.cone (S.diagram ⋙ to_Top)).π.app i) :=
+def iso_limit₂ (S : Profinite.{u}) : S ≅ (Profinite.limit_cone S.diagram).X :=
+  (iso_as_limit_cone_lift S)
+
+def iso_limit_cones (S : Profinite.{u}) : Profinite.limit_cone S.diagram ≅ limit.cone S.diagram :=
+  is_limit.unique_up_to_iso (Profinite.limit_cone_is_limit S.diagram) (limit.is_limit S.diagram)
+
+def iso_limit₃ (S : Profinite.{u}) : (Profinite.limit_cone S.diagram).X ≅ (limit.cone S.diagram).X :=
+  iso_X_of_iso_cones (iso_limit_cones S)
+
+def iso_limit (S : Profinite.{u}) : S ≅ limit S.diagram :=
+  as_iso ((iso_limit₂ S).hom ≫ (iso_limit₃ S).hom)
+
+def to_Top_map_cone_iso_limit_cone (S : Profinite.{u}) :
+  (limit.cone (S.diagram ⋙ to_Top)) ≅ (to_Top.map_cone S.as_limit_cone) :=
 begin
   have : preserves_limit S.diagram to_Top := by apply_instance,
   let c := this.preserves S.as_limit,
-  have h : (limit.cone (S.diagram ⋙ to_Top)) ≅ (to_Top.map_cone S.as_limit_cone) :=
-   is_limit.unique_up_to_iso (limit.is_limit _) c,
-  -- have hX : (limit.cone (S.diagram ⋙ to_Top)).X ≅ (to_Top.map_cone S.as_limit_cone).X,
-  -- { exact ⟨h.hom, h.inv⟩, },
-  have h₁ : ((limit.cone (S.diagram ⋙ to_Top)).π.app i) =
-    h.hom.hom ≫ ((to_Top.map_cone S.as_limit_cone).π.app i) :=
-    (cone_morphism.w h.hom i).symm,
-  rw h₁,
-  haveI h₂ : is_iso h.hom.hom,
-  { fconstructor,
+  exact is_limit.unique_up_to_iso (limit.is_limit _) c,
+end
+
+instance to_Top_map_cone_iso_limit_cone_hom_iso (S : Profinite.{u}) :
+  is_iso (to_Top_map_cone_iso_limit_cone S).hom.hom :=
+begin
+  let h := to_Top_map_cone_iso_limit_cone S,
+  fconstructor,
     use h.inv.hom,
     split,
     { have hom_comp : (h.hom.hom ≫ h.inv.hom) = (h.hom ≫ h.inv).hom := by refl,
@@ -46,7 +53,24 @@ begin
       rw hom_comp,
       rw id_comp,
       rw ← cone_morphism.ext_iff _ _,
-      exact h.inv_hom_id' } },
+      exact h.inv_hom_id' },
+end
+
+def to_Top_iso_limit (S : Profinite.{u}) : to_Top.obj S ≅ limit (S.diagram ⋙ to_Top) :=
+  (as_iso (to_Top_map_cone_iso_limit_cone S).hom.hom).symm
+  -- iso_X_of_iso_cones (to_Top_map_cone_iso_limit_cone S)
+
+def to_Top_iso_limit' (S : Profinite.{u}) : to_Top.obj (limit S.diagram) ≅ limit (S.diagram ⋙ to_Top) :=
+  preserves_limit_iso to_Top S.diagram
+
+instance proj_epi {S : Profinite.{u}} (i : discrete_quotient S) :
+  epi ((limit.cone (S.diagram ⋙ to_Top)).π.app i) :=
+begin
+  let h := to_Top_map_cone_iso_limit_cone S,
+  have h₁ : ((limit.cone (S.diagram ⋙ to_Top)).π.app i) =
+    h.hom.hom ≫ ((to_Top.map_cone S.as_limit_cone).π.app i) :=
+    (cone_morphism.w h.hom i).symm,
+  rw h₁,
   have h₃ := is_iso.epi_of_iso h.hom.hom,
   apply epi_comp h.hom.hom _,
   simp only [Profinite.to_Top_map, category_theory.functor.map_cone_π_app],
@@ -54,20 +78,56 @@ begin
   exact discrete_quotient.proj_surjective i,
 end
 
-def map_as_discrete_quotient {α β : Type u} {f : α → β} [topological_space α] [topological_space β]
-  [compact_space α] [t2_space α] [totally_disconnected_space α] [discrete_topology β]
-  (cont : continuous f) : discrete_quotient α := discrete_quotient.comap ⊥ cont
+def map_as_discrete_quotient {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj S ⟶ X) : discrete_quotient S := discrete_quotient.comap ⊥ g.continuous_to_fun
+
+def map_as_discrete_quotient' {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj (limit S.diagram) ⟶ X) : discrete_quotient S :=
+  discrete_quotient.comap ⊥ (to_Top.map (iso_limit S).hom ≫ g).continuous_to_fun
+
+def proj_g {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X] (g : to_Top.obj S ⟶ X) :
+  S ⟶ (S.diagram.obj (map_as_discrete_quotient g)) :=
+  ⟨(map_as_discrete_quotient g).proj, discrete_quotient.proj_continuous _⟩
+
+def proj_g' {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj (limit S.diagram) ⟶ X) :
+  limit S.diagram ⟶ (S.diagram.obj (map_as_discrete_quotient' g)) :=
+limit.π S.diagram (opposite.unop (opposite.op (map_as_discrete_quotient' g)))
+
+def incl_im {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X] (g : to_Top.obj S ⟶ X) :
+  (S.diagram ⋙ to_Top).obj (map_as_discrete_quotient g) ⟶ X := sorry
+
+def incl_im' {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj (limit S.diagram) ⟶ X) :
+  (S.diagram ⋙ to_Top).obj (map_as_discrete_quotient' g) ⟶ X := sorry
+
+lemma g_factorisation {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj S ⟶ X) : g = to_Top.map (proj_g g) ≫ (incl_im g) := sorry
+
+lemma g_factorisation' {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X]
+  (g : to_Top.obj (limit S.diagram) ⟶ X) :
+  g = (proj_g' g) ≫ (incl_im' g) := sorry
 
 lemma discreteness_condition_necessary {S : Profinite.{u}} {X : Top.{u}} [discrete_topology X] :
   is_iso (can_map_from_colim_of_homs_to_hom_from_limit X (S.diagram ⋙ Profinite.to_Top)) :=
 begin
-  refine (is_iso_iff_bijective (can_map_from_colim_of_homs_to_hom_from_limit X (S.diagram ⋙ Profinite.to_Top))).mpr _,
+  refine (is_iso_iff_bijective (can_map_from_colim_of_homs_to_hom_from_limit X
+    (S.diagram ⋙ to_Top))).mpr _,
   split, { exact can_is_injective _ _ (λ i, proj_epi i), },
   intros f,
-  rw ← to_Top_iso_limit S at f,
-  cases f,
-  cases f_1,
-  let a := colimit.ι (((S.diagram ⋙ to_Top).op ⋙ yoneda.obj X ⋙ ulift_functor))
-    (opposite.op (map_as_discrete_quotient f_1_continuous_to_fun)),
-  sorry,
+  let g := (to_Top_iso_limit' S).hom ≫ f.down,
+  let a := colimit.ι ((S.diagram ⋙ to_Top).op ⋙ yoneda.obj X ⋙ ulift_functor)
+    (opposite.op (map_as_discrete_quotient' g)) ⟨(incl_im' g)⟩,
+  use a,
+  rw ← ulift.down_inj,
+  rw ← can_eq_left_comp_with_proj _ _ _,
+  simp only [],
+  rw ← iso.cancel_iso_hom_left (to_Top_iso_limit' S) _ _,
+  change (to_Top_iso_limit' S).hom ≫ f.down with g,
+  have : to_Top_iso_limit' S = preserves_limit_iso to_Top S.diagram := by refl,
+  rw this,
+  simp only [← category.assoc'],
+  rw preserves_limits_iso_hom_π to_Top S.diagram _,
+  simp only [Profinite.to_Top_map],
+  exact (g_factorisation' g).symm,
 end
