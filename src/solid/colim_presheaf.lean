8bccf5657b22b6c2534edb5ad0e84d91885dd865
dagurtomas <dagurtomas@gmail.com>
2023-03-02 14:57:48 +0000

    no real progress here

--- src/solid/colim_presheaf.lean
+++ src/solid/colim_presheaf.lean
@@ -6,7 +6,7 @@ import solid.has_limits_fintype
 
 noncomputable theory
 
-universes u
+universes v u
 
 open category_theory
 open Profinite
@@ -29,10 +29,25 @@ begin
   exact has_colimits_of_size.has_colimits_of_shape (costructured_arrow to_Profinite.op S),
 end
 
+-- instance (S : Profinite.{u}ᵒᵖ) : has_colimits_of_shape
+--   (costructured_arrow to_Profinite.op S) (Type (max v u)) :=
+-- begin
+--   haveI : has_colimits_of_size.{(max v u)} (Type (max v u)) := by apply_instance,
+--   haveI : has_colimits_of_size.{u (max v u)} (Type (max v u)) :=
+--     has_colimits_of_size_shrink.{u (max v u) (max v u) (max v u)} (Type (max v u)),
+--   exact has_colimits_of_size.has_colimits_of_shape (costructured_arrow to_Profinite.op S),
+-- end
+
 set_option pp.universes false
 
 def colim_as_Kan_extension (X : Type (u+1)) : Profinite.{u}ᵒᵖ ⥤ Type (u+1) :=
   (Lan to_Profinite.op).obj (Fintype.incl.op ⋙ ulift_functor.{u+1}.op ⋙ (yoneda.obj X))
+
+-- variables X : Type v
+-- #check yoneda.obj X
+
+-- def colim_as_Kan_extension' (X : Type v) : Profinite.{u}ᵒᵖ ⥤ Type (max v u) :=
+--   (Lan to_Profinite.op).obj (Fintype.incl.op ⋙ ulift_functor.op ⋙ (yoneda.obj X))
 
 instance discrete_quotient_cofiltered (S : Profinite.{u}) :
   is_cofiltered (discrete_quotient S) := by fsplit
@@ -51,53 +66,6 @@ instance : faithful to_Profinite.{u} := by constructor
 instance structured_arrow_cofiltered_nonempty (S : Profinite.{u}) [nonempty S] :
   is_cofiltered (structured_arrow S to_Profinite) :=
 is_cofiltered.of_left_adjoint (dq_sa_adjunction S)
-
-instance (S : Profinite.{u}) [is_empty S] : faithful (structured_arrow.proj S to_Profinite) :=
-  by apply_instance
-
-lemma maps_out_of_empty {S T : Profinite.{u}} [is_empty S] (f g : S ⟶ T) : f = g :=
-begin
-  ext,
-  rw is_empty_iff at _inst_1,
-  exfalso,
-  exact _inst_1 a,
-end
-
-instance (S : Profinite.{u}) [is_empty S] : full (structured_arrow.proj S to_Profinite) :=
-  functor.full_of_surjective _ (λ f g p,
-begin
-  dsimp at *,
-  have : f.hom ≫ to_Profinite.map p = g.hom,
-  { ext,
-    dsimp at *,
-    exfalso,
-    rw is_empty_iff at _inst_1,
-    exact _inst_1 a, },
-  use structured_arrow.hom_mk p this,
-  refl,
-end)
-
-instance (S : Profinite.{u}) [is_empty S] : ess_surj (structured_arrow.proj S to_Profinite) :=
-begin
-  constructor,
-  intros Y,
-  let f : S → to_Profinite.obj Y := is_empty.elim' _inst_1,
-  have : continuous f,
-  { rw continuous_def,
-    intros U hU,
-    rw set.eq_empty_of_is_empty (f ⁻¹' U),
-    exact is_open_empty },
-  let f' : S ⟶ to_Profinite.obj Y := ⟨f, this⟩,
-  use structured_arrow.mk f',
-  use iso.refl Y,
-end
-
-instance (S : Profinite.{u}) [is_empty S] : is_equivalence (structured_arrow.proj S to_Profinite) :=
-  equivalence.of_fully_faithfully_ess_surj (structured_arrow.proj S to_Profinite)
-
-def sa_empty_equiv_Fintype (S : Profinite.{u}) [is_empty S] :
-  structured_arrow S to_Profinite ≌ Fintype :=
-functor.as_equivalence (structured_arrow.proj S to_Profinite)
 
 instance : is_cofiltered Fintype := category_theory.cofiltered_of_has_finite_limits
 
@@ -122,8 +90,35 @@ is_filtered.of_equivalence (structured_arrow_op_equivalence to_Profinite S.unop)
 
 instance (X : Type (u+1)) : preserves_finite_limits (colim_as_Kan_extension X) :=
 begin
+  apply preserves_finite_limits_of_preserves_finite_limits_of_size,
+  introsI K _ _,
+  -- dsimp [colim_as_Kan_extension],
+  constructor,
+  intros F,
+  -- dsimp [Lan],
+  -- dsimp [Lan.loc],
+  constructor,
+  intros c hc,
   sorry,
+  -- type_check Lan.diagram.{u} to_Profinite.op
+  --   (Fintype.incl.op ⋙ ulift_functor.{u+1}.op ⋙ (yoneda.obj X)) (op Profinite.empty),
+  -- exact limits.filtered_colim_preserves_finite_limits,
+  -- constructor,
+  -- dsimp,
+  -- intros J hs hf,
+  -- unfold colim_as_Kan_extension,
 end
+
+variables (S : Profinite.{u}ᵒᵖ)
+
+instance (S : Profinite.{u}ᵒᵖ) : large_category (costructured_arrow to_Profinite.op S) :=
+  by apply_instance
+
+instance (S : Profinite.{u}ᵒᵖ) : small_category (discrete_quotient S.unop)ᵒᵖ := by apply_instance
+
+-- This is the problem. Once we can index the colimit in a small category, we're much better off.
+
+set_option pp.universes true
 
 def discrete_as_Kan_extension (X : Type (u+1)) : CondensedSet.{u} :=
 { val := colim_as_Kan_extension X,
@@ -132,7 +127,16 @@ def discrete_as_Kan_extension (X : Type (u+1)) : CondensedSet.{u} :=
     rw is_sheaf_iff_is_sheaf_of_type,
     rw (functor.is_proetale_sheaf_of_types_tfae (colim_as_Kan_extension X)).out 0 5,
     refine ⟨_,_,_⟩,
-    { sorry },
+    { dsimp [functor.empty_condition],
+      dsimp [colim_as_Kan_extension],
+      split,
+      { rintros a b h,
+        -- obtain ⟨j, a, ha⟩ := types.jointly_surjective'.{u+1 u} a,
+        sorry,  },
+      { sorry, }
+      -- change (colim_as_Kan_extension X).obj (op empty) with
+      --   to_Profinite.op.obj (op (Fintype.of empty)),
+       },
     { sorry },
     { sorry },
   end, }
